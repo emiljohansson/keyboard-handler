92a56afa8ff72aeb1f897b9932e1c5c338ed6f59
Emil Johansson <emijohansson@deloitte.com>
2018-05-13 08:43:44 -0600

    update sandbox. lint and arrow functions

--- sandbox/app.js
+++ sandbox/app.js
@@ -1,19 +1,20 @@
-'use strict';
+console.log('app')
 
-const keyboard = require('../');
+const keyboard = require('../')
 
-keyboard.keyPressed(function(e) {
-    // console.log('pressed', e.key, e.keyCode);
-});
+keyboard.keyPressed(e => {
+  console.log('pressed', e.key, e.keyCode)
+})
 
-keyboard.keyReleased(function(e) {
-    // console.log('released', e.key, e.keyCode);
-});
+keyboard.keyReleased(e => {
+  console.log('released', e.key, e.keyCode)
+})
 
-keyboard.keyIsDown(keyboard.codes.up, function(e) {
-    // console.log('isdown', e.key, e.keyCode);
-});
+keyboard.keyIsDown(keyboard.codes.up, e => {
+  console.log('isdown', e.key, e.keyCode)
+})
 
-keyboard.keysAreDown([69, 81, 87], function() {
-    // console.log('combination');
-});
+// q+w+e
+keyboard.keysAreDown([69, 81, 87], () => {
+  console.log('combination')
+})

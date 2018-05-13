1e4840ec243c1e68044abcbb36134d00490a9385
Emil Johansson <emijohansson@deloitte.com>
2018-05-13 08:06:24 -0600

    standard lint

--- index.js
+++ index.js
@@ -1,129 +1,127 @@
-'use strict';
-
 exports.codes = {
-    'backspace': 8,
-    'tab': 9,
-    'enter': 13,
-    'shift': 16,
-    'control': 17,
-    'alt': 18,
-    'caps_lock': 20,
-    'escape': 27,
-    'space': 32,
-    'page_up': 33,
-    'page_down': 34,
-    'end': 35,
-    'home': 36,
-    'left': 37,
-    'up': 38,
-    'right': 39,
-    'down': 40,
-    'delete': 46
-};
+  'backspace': 8,
+  'tab': 9,
+  'enter': 13,
+  'shift': 16,
+  'control': 17,
+  'alt': 18,
+  'caps_lock': 20,
+  'escape': 27,
+  'space': 32,
+  'page_up': 33,
+  'page_down': 34,
+  'end': 35,
+  'home': 36,
+  'left': 37,
+  'up': 38,
+  'right': 39,
+  'down': 40,
+  'delete': 46
+}
 
-var types = {
-    down: 'keydown',
-    up: 'keyup'
-};
-var events = {};
-var keyDownEvents;
-var codeCache = {};
+const types = {
+  down: 'keydown',
+  up: 'keyup'
+}
+const events = {}
+const codeCache = {}
+let keyDownEvents
 
-exports.keysAreDown = function(codes, cb) {
-    initEvent(types.up);
-    exports.keyPressed(function(event) {
-        if (!every(codes, function(code) {
-                return codeCache[code] === true;
-            })) {
-            return;
-        }
-        cb();
-    });
-};
-
-exports.keyIsDown = function(code, cb) {
-    if (keyDownEvents) {
-        if (!keyDownEvents[code]) {
-            keyDownEvents[code] = [cb];
-            return;
-        }
-        keyDownEvents[code].push(cb);
-        return;
+exports.keysAreDown = function (codes, cb) {
+  initEvent(types.up)
+  exports.keyPressed(function (event) {
+    if (!every(codes, function (code) {
+      return codeCache[code] === true
+    })) {
+      return
     }
-    keyDownEvents = {};
-    keyDownEvents[code] = [cb];
-    exports.keyPressed(onKeyIsDown);
-};
-
-exports.keyPressed = function(cb) {
-    initEvent(types.down);
-    events[types.down].push(cb);
-};
-
-exports.keyReleased = function(cb) {
-    initEvent(types.up);
-    events[types.up].push(cb);
-};
-
-function getKey(event) {
-    return event.keyCode || event.which;
+    cb()
+  })
 }
 
-function onKeyIsDown(event) {
-    var callbacks = keyDownEvents[getKey(event)];
-    if (!callbacks) {
-        return;
+exports.keyIsDown = function (code, cb) {
+  if (keyDownEvents) {
+    if (!keyDownEvents[code]) {
+      keyDownEvents[code] = [cb]
+      return
     }
-    forEach(callbacks, bind(caller, event));
+    keyDownEvents[code].push(cb)
+    return
+  }
+  keyDownEvents = {}
+  keyDownEvents[code] = [cb]
+  exports.keyPressed(onKeyIsDown)
 }
 
-function bind(cb, thisArg) {
-    return function() {
-        var args = Array.prototype.slice.call(arguments);
-        args.push(thisArg);
-        cb.apply(thisArg, args);
-    };
+exports.keyPressed = function (cb) {
+  initEvent(types.down)
+  events[types.down].push(cb)
 }
 
-function every(array, predicate) {
-    var length = array.length;
-    var index = -1;
-    while (++index < length) {
-        if (predicate(array[index]) === false) {
-            return false;
-        }
+exports.keyReleased = function (cb) {
+  initEvent(types.up)
+  events[types.up].push(cb)
+}
+
+function getKey (event) {
+  return event.keyCode || event.which
+}
+
+function onKeyIsDown (event) {
+  var callbacks = keyDownEvents[getKey(event)]
+  if (!callbacks) {
+    return
+  }
+  forEach(callbacks, bind(caller, event))
+}
+
+function bind (cb, thisArg) {
+  return function () {
+    var args = Array.prototype.slice.call(arguments)
+    args.push(thisArg)
+    cb.apply(thisArg, args)
+  }
+}
+
+function every (array, predicate) {
+  var length = array.length
+  var index = -1
+  while (++index < length) {
+    if (predicate(array[index]) === false) {
+      return false
     }
-    return true;
+  }
+  return true
 }
 
-function forEach(array, iteratee) {
-    var length = array.length;
-    var index = -1;
-    while (++index < length) {
-        iteratee(array[index]);
+function forEach (array, iteratee) {
+  var length = array.length
+  var index = -1
+  while (++index < length) {
+    iteratee(array[index])
+  }
+}
+
+function initEvent (type) {
+  if (events[type]) {
+    return
+  }
+  events[type] = []
+  document.addEventListener(type, on(events[type]))
+}
+
+function on (array) {
+  return function (event) {
+    if (event.type === types.down) {
+      codeCache[getKey(event)] = true
     }
-}
-
-function initEvent(type) {
-    if (events[type]) {
-        return;
+    if (event.type === types.up) {
+      delete codeCache[getKey(event)]
     }
-    events[type] = [];
-    document.addEventListener(type, on(events[type]));
+    forEach(array, bind(caller, event))
+  }
 }
 
-function on(array) {
-    return function(event) {
-        if (event.type === types.down) {
-            codeCache[getKey(event)] = true;
-        }
-        if (event.type === types.up) {
-            delete codeCache[getKey(event)];
-        }
-        forEach(array, bind(caller, event));
-    };
-}
-
-function caller(cb, event) {
-    cb(event);
+function caller (cb, event) {
+  cb(event)
 }
--- test.js
+++ test.js
@@ -1,138 +1,130 @@
-'use strict';
+import test from 'ava'
+import sinon from 'sinon'
+import keyboard from './'
 
-import test from 'ava';
-import sinon from 'sinon';
-import keyboard from './';
+global.document = require('jsdom').jsdom('<body></body>')
+global.window = document.defaultView
+global.navigator = window.navigator
 
-global.document = require('jsdom').jsdom('<body></body>');
-global.window = document.defaultView;
-global.navigator = window.navigator;
-
-function simulateKeyEvent(code, type, keyKey) {
-    var KeyboardEventInit = {
-        bubbles: true,
-        cancelable: true,
-        shiftKey: false
-    };
-    keyKey = keyKey || 'keyCode';
-    KeyboardEventInit[keyKey] = code;
-    var e = new window.KeyboardEvent(type, KeyboardEventInit);
-    document.dispatchEvent(e);
-}
-
-function trigger(type, code) {
-    var e = new window.KeyboardEvent();
-    document.dispatchEvent(e);
+function simulateKeyEvent (code, type, keyKey) {
+  var KeyboardEventInit = {
+    bubbles: true,
+    cancelable: true,
+    shiftKey: false
+  }
+  keyKey = keyKey || 'keyCode'
+  KeyboardEventInit[keyKey] = code
+  var e = new window.KeyboardEvent(type, KeyboardEventInit)
+  document.dispatchEvent(e)
 }
 
 test('exist', (t) => {
-    const expected = 'object';
-    t.is(typeof keyboard, 'object');
-});
+  t.is(typeof keyboard, 'object')
+})
 
 test('have functions', (t) => {
-    const expected = 'function';
-    t.is(typeof keyboard.keysAreDown, expected);
-    t.is(typeof keyboard.keyIsDown, expected);
-    t.is(typeof keyboard.keyPressed, expected);
-    t.is(typeof keyboard.keyReleased, expected);
-});
+  const expected = 'function'
+  t.is(typeof keyboard.keysAreDown, expected)
+  t.is(typeof keyboard.keyIsDown, expected)
+  t.is(typeof keyboard.keyPressed, expected)
+  t.is(typeof keyboard.keyReleased, expected)
+})
 
 test('keyPressed calls callback', (t) => {
-    const callback = sinon.spy();
-    keyboard.keyPressed(callback);
-    simulateKeyEvent(13, 'keydown');
-    const actual = callback.called;
-    t.true(actual);
+  const callback = sinon.spy()
+  keyboard.keyPressed(callback)
+  simulateKeyEvent(13, 'keydown')
+  const actual = callback.called
+  t.true(actual)
 
-    simulateKeyEvent(13, 'keyup');
-});
+  simulateKeyEvent(13, 'keyup')
+})
 
 test('keyPressed passes event object', (t) => {
-    const expected = 15;
-    keyboard.keyPressed(function(event) {
-        const actual = event.keyCode;
-        t.is(actual, expected);
-    });
-    simulateKeyEvent(expected, 'keydown');
-});
+  const expected = 15
+  keyboard.keyPressed(function (event) {
+    const actual = event.keyCode
+    t.is(actual, expected)
+  })
+  simulateKeyEvent(expected, 'keydown')
+})
 
 test('keyPressed passes event.which object', (t) => {
-    const expected = 15;
-    keyboard.keyPressed(function(event) {
-        const actual = event.which;
-        t.is(actual, expected);
-    });
-    simulateKeyEvent(expected, 'keydown', 'which');
-});
+  const expected = 15
+  keyboard.keyPressed(function (event) {
+    const actual = event.which
+    t.is(actual, expected)
+  })
+  simulateKeyEvent(expected, 'keydown', 'which')
+})
 
 test('keyReleased calls callback', (t) => {
-    const callback = sinon.spy();
-    keyboard.keyReleased(callback);
-    simulateKeyEvent(13, 'keydown');
-    simulateKeyEvent(13, 'keyup');
-    const actual = callback.called;
-    t.true(actual);
-});
+  const callback = sinon.spy()
+  keyboard.keyReleased(callback)
+  simulateKeyEvent(13, 'keydown')
+  simulateKeyEvent(13, 'keyup')
+  const actual = callback.called
+  t.true(actual)
+})
 
 test('keyIsDown calls callback', (t) => {
-    const callback = sinon.spy();
-    keyboard.keyIsDown(12, callback);
-    keyboard.keyIsDown(13, callback);
-    simulateKeyEvent(13, 'keydown');
-    const actual = callback.callCount;
-    const expected = 1;
-    t.is(actual, expected);
+  const callback = sinon.spy()
+  keyboard.keyIsDown(12, callback)
+  keyboard.keyIsDown(13, callback)
+  simulateKeyEvent(13, 'keydown')
+  const actual = callback.callCount
+  const expected = 1
+  t.is(actual, expected)
 
-    simulateKeyEvent(13, 'keyup');
-});
+  simulateKeyEvent(13, 'keyup')
+})
 
 test('keyIsDown calls callbacks', (t) => {
-    const callback = sinon.spy();
-    keyboard.keyIsDown(13, callback);
-    keyboard.keyIsDown(13, callback);
-    simulateKeyEvent(13, 'keydown');
-    const actual = callback.callCount;
-    const expected = 2;
-    t.is(actual, expected);
+  const callback = sinon.spy()
+  keyboard.keyIsDown(13, callback)
+  keyboard.keyIsDown(13, callback)
+  simulateKeyEvent(13, 'keydown')
+  const actual = callback.callCount
+  const expected = 2
+  t.is(actual, expected)
 
-    simulateKeyEvent(13, 'keyup');
-});
+  simulateKeyEvent(13, 'keyup')
+})
 
 test('keyIsDown NOT call callback', (t) => {
-    const callback = sinon.spy();
-    keyboard.keyIsDown(14, callback);
-    simulateKeyEvent(13, 'keydown');
-    const actual = callback.called;
-    t.false(actual);
+  const callback = sinon.spy()
+  keyboard.keyIsDown(14, callback)
+  simulateKeyEvent(13, 'keydown')
+  const actual = callback.called
+  t.false(actual)
 
-    simulateKeyEvent(13, 'keyup');
-});
+  simulateKeyEvent(13, 'keyup')
+})
 
 test('keysAreDown calls callback', (t) => {
-    const callback = sinon.spy();
-    keyboard.keysAreDown([13, 14, 15], callback);
-    simulateKeyEvent(13, 'keydown');
-    simulateKeyEvent(14, 'keydown');
-    simulateKeyEvent(15, 'keydown');
-    const actual = callback.called;
-    const expected = true;
-    t.is(actual, expected);
+  const callback = sinon.spy()
+  keyboard.keysAreDown([13, 14, 15], callback)
+  simulateKeyEvent(13, 'keydown')
+  simulateKeyEvent(14, 'keydown')
+  simulateKeyEvent(15, 'keydown')
+  const actual = callback.called
+  const expected = true
+  t.is(actual, expected)
 
-    simulateKeyEvent(13, 'keyup');
-    simulateKeyEvent(14, 'keyup');
-    simulateKeyEvent(15, 'keyup');
-});
+  simulateKeyEvent(13, 'keyup')
+  simulateKeyEvent(14, 'keyup')
+  simulateKeyEvent(15, 'keyup')
+})
 
 test('keysAreDown NOT call callback', (t) => {
-    const callback = sinon.spy();
-    keyboard.keysAreDown([13, 14, 15], callback);
-    simulateKeyEvent(13, 'keydown');
-    simulateKeyEvent(15, 'keydown');
-    const actual = callback.called;
-    const expected = false;
-    t.is(actual, expected);
+  const callback = sinon.spy()
+  keyboard.keysAreDown([13, 14, 15], callback)
+  simulateKeyEvent(13, 'keydown')
+  simulateKeyEvent(15, 'keydown')
+  const actual = callback.called
+  const expected = false
+  t.is(actual, expected)
 
-    simulateKeyEvent(13, 'keyup');
-    simulateKeyEvent(15, 'keyup');
-});
+  simulateKeyEvent(13, 'keyup')
+  simulateKeyEvent(15, 'keyup')
+})
